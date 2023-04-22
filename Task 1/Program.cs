c68f21352e962356d921314499b901b2f4fc94a7
pavel <pavel2003872@gmail.com>
2023-04-22 20:48:50 +0300

    add my task

---
+++ Task 1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static int[] FillArray()
+        {
+            Console.WriteLine("Ввеліть к-ть елементі масиву");
+            int arrayLength = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть елементи масиву, розділені пробілами: ");
+            string input = Console.ReadLine(); 
+            string[] strArr = input.Split(); 
+
+            int[] arr = new int[strArr.Length];
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                arr[i] = int.Parse(strArr[i]); 
+            }
+            return arr;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = FillArray();
+            Console.Write("Введіть елемент масиву з якого начнеться видалення: ");
+            int k = int.Parse(Console.ReadLine());
+            Console.Write("Введіть к-ть елементів яку хочите видалити: ");
+            int t = int.Parse(Console.ReadLine());
+
+            RemoveElementsFromArray(ref arr, k, t);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+        static void RemoveElementsFromArray(ref int[] arr, int k, int t)
+        {
+            int elementsToRemove = Math.Min(t, arr.Length - k); // це к-ть ел дня видалення
+
+            if (elementsToRemove <= 0) // той випадок якщо елементів нема
+            {
+                return;
+            }
+
+            int newLength = arr.Length - elementsToRemove; // нова довжина масиву
+            int[] newArr = new int[newLength];
+
+            for (int i = 0; i < k; i++) // переношу чистину масиву
+            {
+                newArr[i] = arr[i];
+            }
+
+            for (int i = k + elementsToRemove; i < arr.Length; i++) 
+            {
+                newArr[i - elementsToRemove] = arr[i]; // копія масиву вже після видалення
+            }
+
+            arr = newArr; 
+        }
+    }
+}
---
+++ task_2_v4/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть розмірність масиву (рядки стовпці): ");
+            string[] size = Console.ReadLine().Split();
+            int rows = int.Parse(size[0]);
+            int cols = int.Parse(size[1]);
+
+            int[][] array = new int[rows][];
+
+            Console.WriteLine($"Введіть {rows} рядків по {cols} елементів кожен:");
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write($"Рядок {i + 1}: ");
+                string[] values = Console.ReadLine().Split();
+
+                array[i] = new int[cols];
+
+                for (int j = 0; j < cols; j++)
+                {
+                    if (j < values.Length)
+                    {
+                        array[i][j] = int.Parse(values[j]);
+                    }
+                    else
+                    {
+                        array[i][j] = 0;
+                    }
+                }
+            }
+            Console.WriteLine("Масив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(array[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Введіть індекс рядка, який потрібно видалити: ");
+            int k = int.Parse(Console.ReadLine());
+
+            int[][] newArray = new int[rows - 1][];
+            int index = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (i != k)
+                {
+                    newArray[index] = new int[cols];
+
+                    for (int j = 0; j < cols; j++)
+                    {
+                        newArray[index][j] = array[i][j];
+                    }
+
+                    index++;
+                }
+            }
+            Console.WriteLine("Масив після видалення:");
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(newArray[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
