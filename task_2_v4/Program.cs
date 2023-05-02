4e9cd5cea9000ce1c82dab3812518ba98726b29d
DZENXI <107136277+DZENXI@users.noreply.github.com>
2023-05-02 18:40:09 +0300

    Update Program.cs
    added random filling

--- task_2_v4/Program.cs
+++ task_2_v4/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Task_2
 {
@@ -10,13 +6,49 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            int[][] array;
+            Console.WriteLine("Виберіть метод заповнення масиву: ");
+            Console.WriteLine("1 - ввести в ручну;");
+            Console.WriteLine("2 - заповнити рандомно;");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    array = ReadArrayFromConsole();
+
+                    break;
+                case 2:
+                    Console.Write("Введіть розмірність масиву (рядки стовпці): ");
+                    string[] size = Console.ReadLine().Split();
+                    int rows = int.Parse(size[0]);
+                    int cols = int.Parse(size[1]);
+                    array = RandomFillArray(rows, cols);
+                    break;
+                default:
+                    Console.WriteLine("Неправильний вибір.");
+                    return;
+
+            }
+            Console.WriteLine("Масив:");
+            PrintArray(array);
+
+            Console.WriteLine("Введіть індекс рядка, який потрібно видалити: ");
+            int k = int.Parse(Console.ReadLine());
+
+            int[][] newArray = RemoveRow(array, k);
+            Console.WriteLine("Масив після видалення:");
+            PrintArray(newArray);
+            Console.ReadKey();
+        }
+
+        private static int[][] ReadArrayFromConsole()
+        {
             Console.Write("Введіть розмірність масиву (рядки стовпці): ");
             string[] size = Console.ReadLine().Split();
             int rows = int.Parse(size[0]);
             int cols = int.Parse(size[1]);
 
             int[][] array = new int[rows][];
-
             Console.WriteLine($"Введіть {rows} рядків по {cols} елементів кожен:");
             for (int i = 0; i < rows; i++)
             {
@@ -24,7 +56,6 @@ namespace Task_2
                 string[] values = Console.ReadLine().Split();
 
                 array[i] = new int[cols];
-
                 for (int j = 0; j < cols; j++)
                 {
                     if (j < values.Length)
@@ -37,49 +68,49 @@ namespace Task_2
                     }
                 }
             }
-            Console.WriteLine("Масив:");
-            for (int i = 0; i < rows; i++)
+            return array;
+        }
+
+        private static void PrintArray(int[][] array)
+        {
+            for (int i = 0; i < array.Length; i++)
             {
-                for (int j = 0; j < cols; j++)
+                for (int j = 0; j < array[i].Length; j++)
                 {
                     Console.Write(array[i][j] + " ");
                 }
                 Console.WriteLine();
             }
+        }
 
-            Console.WriteLine("Введіть індекс рядка, який потрібно видалити: ");
-            int k = int.Parse(Console.ReadLine());
-
-            int[][] newArray = new int[rows - 1][];
+        private static int[][] RemoveRow(int[][] array, int k)
+        {
+            int[][] newArray = new int[array.Length - 1][];
             int index = 0;
 
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 if (i != k)
                 {
-                    newArray[index] = new int[cols];
-
-                    for (int j = 0; j < cols; j++)
-                    {
-                        newArray[index][j] = array[i][j];
-                    }
-
+                    newArray[index] = array[i];
                     index++;
                 }
             }
-            Console.WriteLine("Масив після видалення:");
-            for (int i = 0; i < newArray.Length; i++)
+            return newArray;
+        }
+        private static int[][] RandomFillArray(int rows, int cols)
+        {
+            Random rnd = new Random();
+            int[][] arr = new int[rows][];
+            for (int i = 0; i < rows; i++)
             {
+                arr[i] = new int[cols];
                 for (int j = 0; j < cols; j++)
                 {
-                    Console.Write(newArray[i][j] + " ");
+                    arr[i][j] = rnd.Next(11);
                 }
-                Console.WriteLine();
             }
-
-            Console.ReadKey();
-
+            return arr;
         }
-
     }
 }
